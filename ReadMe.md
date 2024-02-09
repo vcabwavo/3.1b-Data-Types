0ef46bc1b9096e16b7b19dddabd70c0b8f38ccad
Eric Winebrenner <ejwinebrenner@gmail.com>
2024-02-09 13:39:42 -0500

    base notes

---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+### Before Getting Started
+There are two different branches associated with this lesson.
+- Main
+  - This branch provides the base notes of material not filled out.
+- Completed
+  - This branch includes all the added notes for you to review and compare with your own code-along.
+
+### Topics Covered
+- Data Types
+  - How to use a variable
+  - Booleans, Null, Undefined, Numbers, Strings, Objects, Arrays, Literals, and Special Values
+
+### What Should Already Be Understood
+- Basic HTML / CSS concepts
+- Variables and structure
+- Nodemon
+
+## Files / Folders Provided in this Lesson
+- `index.js`
+
+## Resources
+- [Numbers MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number)
+- [Data Types MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures)
+
---
+++ index.js
@@ -0,0 +1,80 @@
+//! How to Use Variables
+/* 
+    - Helps us reference, change, and/or detail other variables within our data. 
+    - Helps shape functions, expressions, operators, and statements.
+*/
+
+//! Booleans
+/* 
+    - True / False 
+*/
+
+//! Null
+/* 
+    - An empty value.
+*/
+
+//! Undefined
+/* 
+    - no value
+*/
+
+//! Null vs Undefined
+/* 
+    - Null is a container with nothing.
+    - Undefined is a variable never set.
+*/
+
+//! Numbers
+/* 
+    - Literal numbers; however, there are some minor considerations when handling numbers.
+    - Numbers will be rounded at 16 digits
+    - Consider the MDN docs (resource link in ReadMe.md) for a deeper dive.
+*/
+
+//! String
+/* 
+    - Represents text.
+    - Wrapped in single or double quotes
+        Single-Quotes:  'text'
+        Double-quotes:  "text"
+        Back-tic:       `text`
+*/
+
+//! Object
+/* 
+    - Denoted with curly-brackets { }
+    - Stores many values by means of a key/value association
+    - A container that can hold multiple datatypes.
+
+    {
+        key: value
+    }
+*/
+
+//! Array
+/* 
+    - A container that holds a list of items.
+
+    ex:
+    let list = [ 'item1', 'item2', 'item3' ];
+        (1)   (2)         (3)
+    
+    1. name of the array.
+    2. Denoted with square brackets.
+    3. Each item, regardless of datatype, is separated by commas.
+
+    - Is considered a type of object.
+        - arrays are also containers that hold multiple datatypes.
+*/
+
+//! Data Type Literals
+/*  
+    - Literal representation of a fixed value when created.
+*/
+
+//! Special Values
+/* 
+    - NaN: Not a Number
+    - Infinity
+*/
